6b232ca096f556a8948beb52a9432f6b3a72ffaa
utsaverma2001-crypto <legendofaa2001@gmail.com>
2022-09-04 13:07:36 +0530

    changes in login and home pages

--- Frontend/src/Components/login.js
+++ Frontend/src/Components/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./login.css"
-
+import elp1 from "../images/ellipse.png"
 
 
 function Login() {
@@ -12,28 +12,32 @@ function Login() {
 
   };
   return (
-    <div >
+    <div className="head_login">
+      <img src={elp1} style={{position:"absolute",top:"40%",right:"26%",zIndex:"1"}}></img>
+      <div style={{paddingTop:"50px"}}>
+
+      
       <div className="main_login" style={{display:"flex",flexDirection:"column"}}>
         <div style={{fontSize:"50px"}}>Log In</div>
         <div style={{fontSize:"30px",marginTop:"30px"}}>Email</div>
         <div>
-            <input style={{width:"90%",height:"35px",border:"none",marginTop:"10px",outline:"none"}}></input>
+            <input style={{width:"90%",height:"35px",border:"none",marginTop:"10px",outline:"none",borderRadius:"10px"}}></input>
         </div>
         <div style={{fontSize:"30px",marginTop:"30px"}}>Password</div>
         <div>
-            <input style={{width:"90%",height:"35px",border:"none",marginTop:"10px",outline:"none"}}></input>
+            <input style={{width:"90%",height:"35px",border:"none",marginTop:"10px",outline:"none",borderRadius:"10px"}}></input>
         </div>
         <div style={{textAlign:"center",marginTop:"70px",marginRight:"40px"}}>
-           <button type="submit"    style={{width:"300px" ,fontSize:"20px",color:"#FFFFFF",background:"#8860D0",height:"40px",border:"none",outline:"none"}} >
+           <button type="submit"  className="Button"  style={{width:"200px" ,fontSize:"20px",height:"40px",border:"none",outline:"none",borderRadius:"10px"}} >
               Login
             </button>
             <h4 style={{margin:"10px 0"}}>OR</h4>
-            <button type="submit"    style={{width:"200px" ,fontSize:"20px",color:"#FFFFFF",background:"#1E90FF",height:"40px",border:"none",outline:"none"}} >
+            <button type="submit"  className="Button"  style={{width:"200px" ,fontSize:"20px",height:"40px",border:"none",outline:"none",borderRadius:"10px"}} >
               Login With Google
             </button>
         </div>
         
-
+        </div>
       </div>
     </div>
   );
