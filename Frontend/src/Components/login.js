be0847ec863e3857a6ccf53a61cd410c78a7781e
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-09-05 01:04:47 +0530

    prepared forms for the Login Screen

--- Frontend/src/Components/login.js
+++ Frontend/src/Components/login.js
@@ -1,73 +1,76 @@
+// Packages imports
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+
+// Local imports
 import "./login.css";
 import elp1 from "../images/ellipse.png";
 
+// functional component for Login
 function Login() {
-  const [studentEmail, setEmail] = useState("");
-  const [studentPassword, setPassword] = useState("");
+  // Local States
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const formhandler = e => {
-    e.preventDefault();
+  // This disables the submit button if inputs are empty
+  const buttonDisabled = email.length === 0 || password.length === 0;
+
+  // login button will have a backgorund Color depending upon the buttonDisabled state
+  const loginBackColor = buttonDisabled ? "rgba(255, 255, 255, 0.4)" : "dodgerblue";
+
+  // function to handle Login button press
+  const onLoginPress = e => {
+    try {
+      e.preventDefault();
+      console.log(email, password);
+    } catch (error) {}
   };
+
+  // function to handle Google button press
+  const onGooglePress = e => {
+    try {
+      e.preventDefault();
+    } catch (error) {}
+  };
+
+  // renders the component
   return (
     <div className="head_login">
       <div style={{ paddingTop: "50px" }}>
         <div className="main_login" style={{ display: "flex", flexDirection: "column" }}>
-          <img src={elp1} style={{ position: "absolute", top: "40%", right: "-34%", zIndex: "1" }}></img>
+          <img alt="" src={elp1} style={{ position: "absolute", top: "40%", right: "-34%", zIndex: "1" }}></img>
+
           <div style={{ fontSize: "50px" }}>Log In</div>
-          <div style={{ fontSize: "30px", marginTop: "30px" }}>Email</div>
+
+          <div style={styles.fieldTitle}>Email</div>
           <div>
-            <input
-              style={{
-                width: "90%",
-                height: "35px",
-                border: "none",
-                marginTop: "10px",
-                outline: "none",
-                borderRadius: "10px",
-              }}
-            ></input>
+            <input style={styles.inputBox} onChange={e => setEmail(e.target.value)}></input>
           </div>
-          <div style={{ fontSize: "30px", marginTop: "30px" }}>Password</div>
+
+          <div style={styles.fieldTitle}>Password</div>
           <div>
-            <input
-              style={{
-                width: "90%",
-                height: "35px",
-                border: "none",
-                marginTop: "10px",
-                outline: "none",
-                borderRadius: "10px",
-              }}
-            ></input>
+            <input style={styles.inputBox} onChange={e => setPassword(e.target.value)}></input>
           </div>
+
           <div style={{ textAlign: "center", marginTop: "70px", marginRight: "40px" }}>
             <button
               type="submit"
               className="button glass-morph"
-              style={{
-                width: "200px",
-                fontSize: "20px",
-                height: "40px",
-                outline: "none",
-                borderRadius: "10px",
-              }}
+              style={{ ...styles.buttons, backgroundColor: loginBackColor }}
+              onClick={onLoginPress}
+              disabled={buttonDisabled}
             >
               Login
             </button>
+
             <h4 style={{ margin: "10px 0" }}>OR</h4>
+
             <button
               type="submit"
               className="button glass-morph"
-              style={{
-                width: "300px",
-                fontSize: "20px",
-                height: "40px",
-                outline: "none",
-                borderRadius: "10px",
-              }}
+              style={{ ...styles.buttons, width: "300px" }}
+              onClick={onGooglePress}
             >
               <FontAwesomeIcon icon={faGoogle} /> Login With Google
             </button>
@@ -78,4 +81,26 @@ function Login() {
   );
 }
 
+// exporting the Login component
 export default Login;
+
+// styles for the component
+const styles = {
+  inputBox: {
+    width: "90%",
+    height: "35px",
+    border: "none",
+    marginTop: "10px",
+    outline: "none",
+    borderRadius: "10px",
+    paddingLeft: "10px",
+  },
+  buttons: {
+    width: "200px",
+    fontSize: "20px",
+    height: "40px",
+    outline: "none",
+    borderRadius: "10px",
+  },
+  fieldTitle: { fontSize: "30px", marginTop: "30px" },
+};
