ce11b99c4e0032bc0e3849995f0a98a67e86f0b7
raunak_k02 <raunakk728@gmail.com>
2022-09-04 23:39:42 +0530

    feat/patch: adds global button config, changes background, and fixes responsive issues

--- Frontend/src/Components/login.js
+++ Frontend/src/Components/login.js
@@ -1,42 +1,77 @@
 import React, { useState } from "react";
-import "./login.css"
-import elp1 from "../images/ellipse.png"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import "./login.css";
+import elp1 from "../images/ellipse.png";
 
 function Login() {
   const [studentEmail, setEmail] = useState("");
   const [studentPassword, setPassword] = useState("");
- 
-  const formhandler = (e) => {
-    e.preventDefault();
 
+  const formhandler = e => {
+    e.preventDefault();
   };
   return (
     <div className="head_login">
-      <img src={elp1} style={{position:"absolute",top:"40%",right:"26%",zIndex:"1"}}></img>
-      <div style={{paddingTop:"50px"}}>
-
-      
-      <div className="main_login" style={{display:"flex",flexDirection:"column"}}>
-        <div style={{fontSize:"50px"}}>Log In</div>
-        <div style={{fontSize:"30px",marginTop:"30px"}}>Email</div>
-        <div>
-            <input style={{width:"90%",height:"35px",border:"none",marginTop:"10px",outline:"none",borderRadius:"10px"}}></input>
-        </div>
-        <div style={{fontSize:"30px",marginTop:"30px"}}>Password</div>
-        <div>
-            <input style={{width:"90%",height:"35px",border:"none",marginTop:"10px",outline:"none",borderRadius:"10px"}}></input>
-        </div>
-        <div style={{textAlign:"center",marginTop:"70px",marginRight:"40px"}}>
-           <button type="submit"  className="Button"  style={{width:"200px" ,fontSize:"20px",height:"40px",border:"none",outline:"none",borderRadius:"10px"}} >
+      <div style={{ paddingTop: "50px" }}>
+        <div className="main_login" style={{ display: "flex", flexDirection: "column" }}>
+          <img src={elp1} style={{ position: "absolute", top: "40%", right: "-34%", zIndex: "1" }}></img>
+          <div style={{ fontSize: "50px" }}>Log In</div>
+          <div style={{ fontSize: "30px", marginTop: "30px" }}>Email</div>
+          <div>
+            <input
+              style={{
+                width: "90%",
+                height: "35px",
+                border: "none",
+                marginTop: "10px",
+                outline: "none",
+                borderRadius: "10px",
+              }}
+            ></input>
+          </div>
+          <div style={{ fontSize: "30px", marginTop: "30px" }}>Password</div>
+          <div>
+            <input
+              style={{
+                width: "90%",
+                height: "35px",
+                border: "none",
+                marginTop: "10px",
+                outline: "none",
+                borderRadius: "10px",
+              }}
+            ></input>
+          </div>
+          <div style={{ textAlign: "center", marginTop: "70px", marginRight: "40px" }}>
+            <button
+              type="submit"
+              className="button glass-morph"
+              style={{
+                width: "200px",
+                fontSize: "20px",
+                height: "40px",
+                outline: "none",
+                borderRadius: "10px",
+              }}
+            >
               Login
             </button>
-            <h4 style={{margin:"10px 0"}}>OR</h4>
-            <button type="submit"  className="Button"  style={{width:"200px" ,fontSize:"20px",height:"40px",border:"none",outline:"none",borderRadius:"10px"}} >
-              Login With Google
+            <h4 style={{ margin: "10px 0" }}>OR</h4>
+            <button
+              type="submit"
+              className="button glass-morph"
+              style={{
+                width: "300px",
+                fontSize: "20px",
+                height: "40px",
+                outline: "none",
+                borderRadius: "10px",
+              }}
+            >
+              <FontAwesomeIcon icon={faGoogle} /> Login With Google
             </button>
-        </div>
-        
+          </div>
         </div>
       </div>
     </div>
--- Frontend/src/Components/main.js
+++ Frontend/src/Components/main.js
@@ -1,37 +1,65 @@
 import React, { useState } from "react";
-import "./login.css"
-import img1 from "../images/graphic2.png"
+import "./login.css";
+import img1 from "../images/graphic2.png";
 //import img2 from "../images/graphic3.png"
 
 function Home() {
- 
   return (
-    <div className="Home" style={{display:"flex",flexDirection:"column",textAlign:"left",padding:"40px 50px 0px",marginTop:"-7px"}}>
-        <h1>MEETING APP</h1>
-        <div style={{marginTop:"40px"}}>
-           <h2>Lorem ipsum dolor shit.</h2>
-           <h2 style={{marginTop:"-10px"}}>Now for everyone.</h2>
-        </div>
-        
-        <div style={{display:"flex",flexDirection:"row",paddingTop:"10px"}}>
-            <div style={{display:"flex",flexDirection:"column",paddingTop:"10%"}}>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                   etiam ac sapien ac urna luctus lacinia.</p>
-                   <div style={{display:"flex",flexDirection:"row",marginTop:"20px"}}>
-                         <button type="submit"    style={{width:"200px" ,fontSize:"20px",color:"#FFFFFF",background:"#8860D0",height:"40px",border:"none",outline:"none",borderRadius:"15px"}} >
-                           New Meeting
-                         </button>
-                         <button type="submit"    style={{width:"200px",marginLeft:"30px" ,fontSize:"20px",color:"#FFFFFF",background:"#1E90FF",height:"40px",border:"none",outline:"none",borderRadius:"15px"}} >
-                           Enter code or link
-                         </button>
-                   </div>
-       
-            </div>
-            <div>
-                 <img src={img1} style={{width:"85%",marginLeft:"90px"}} alt="image"></img>
-            </div>
+    <div
+      className="Home"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        textAlign: "left",
+        padding: "40px 50px 0px",
+        marginTop: "-7px",
+      }}
+    >
+      <h1>MEETING APP</h1>
+      <div style={{ marginTop: "40px" }}>
+        <h2>Lorem ipsum dolor shit.</h2>
+        <h2 style={{ marginTop: "-10px" }}>Now for everyone.</h2>
+      </div>
 
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "10px" }}>
+        <div style={{ display: "flex", flexDirection: "column", paddingTop: "10%" }}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, etiam ac sapien ac urna luctus lacinia.</p>
+          <div style={{ display: "flex", flexDirection: "row", marginTop: "20px" }}>
+            <button
+              className="button glass-morph"
+              type="submit"
+              style={{
+                width: "200px",
+                fontSize: "20px",
+                color: "#FFFFFF",
+                background: "#8860D088",
+                outline: "none",
+                borderRadius: "15px",
+              }}
+            >
+              New Meeting
+            </button>
+            <button
+              className="button glass-morph"
+              type="submit"
+              style={{
+                width: "250px",
+                marginLeft: "30px",
+                fontSize: "20px",
+                color: "#FFFFFF",
+                background: "#1E90FF88",
+                outline: "none",
+                borderRadius: "15px",
+              }}
+            >
+              Enter code or link
+            </button>
+          </div>
         </div>
+        <div>
+          <img src={img1} style={{ width: "85%", marginLeft: "90px" }} alt="image"></img>
+        </div>
+      </div>
     </div>
   );
 }
