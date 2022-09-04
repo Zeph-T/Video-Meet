6e17b0d49149138b75d0c69087e8a2dfb33380d0
raunak_k02 <raunakk728@gmail.com>
2022-09-04 22:33:25 +0530

    feat: adds route for join_meet screen

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,9 @@
 // components imports
 import "./App.css";
 //import logo from "./logo.svg";
-import Login from "./Components/login"
-import Home from "./Components/main"
+import Login from "./Components/login";
+import Home from "./Components/main";
+import JoinMeet from "./Components/join_meet/JoinMeet";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // default app functional component
 function App() {
@@ -10,13 +11,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-     
-     <Routes>
-       <Route exact path="/" element={<Login />} />
-       <Route path="/home" element={<Home />} />
-      
-     </Routes>
-   </Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/meet/:id" element={<JoinMeet />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
