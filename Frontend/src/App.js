c59b374e8459cb19426406ce5ab25ce400b9f151
raunak_k02 <raunakk728@gmail.com>
2022-09-05 20:39:06 +0530

    resolved conflicg

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,17 @@
 // components imports
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 // Local components imports
 import "./App.css";
 import AuthPage from "./Components/auth";
 import Home from "./Components/main";
-import JoinMeet from "./Components/join_meet/JoinMeet";
-import store from "./store"
+import store from "./store";
 import SignUp from "./Components/signup";
 import JoinMeet from "./Components/join_meet_screen/JoinMeet";
 import Video from "./Components/meeting_screen/Video";
 
 // default app functional component
-const App=()=> {
+const App = () => {
   // render the app
   return (
     <div className="App">
@@ -28,10 +26,9 @@ const App=()=> {
           </Routes>
         </Router>
       </Provider>
-     
     </div>
   );
-}
+};
 
 // exported the app component as default
 export default App;
